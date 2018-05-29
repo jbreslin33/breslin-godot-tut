5f7e87e845ae379c333cc7e4b5d095e95ffe3329
Jim Breslin <jbreslin33@gmail.com>
2018-05-29 15:38:24 -0400

    functions to move player

--- Player.gd
+++ Player.gd
@@ -1,25 +1,40 @@
 extends Area2D
 
-export (int) var SPEED  # how fast the player will move (pixels/sec)
-var screensize  # size of the game window
+signal hit
 
+export (int) var SPEED
+var velocity = Vector2()
+var screensize
 
 func _ready():
-    screensize = get_viewport_rect().size
+	hide()
+	screensize = get_viewport_rect().size
+
+func start(pos):
+	position = pos
+	show()
+	$Collision.disabled = false
+
+func _process(delta):
+	velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * SPEED
+		$AnimatedSprite.play()
 	
-	
-	func _process(delta):
-    var velocity = Vector2() # the player's movement vector
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * SPEED
-        $AnimatedSprite.play()
-    else:
-        $AnimatedSprite.stop()
+	else:
+		$AnimatedSprite.stop()
+		
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screensize.x)
+	position.y = clamp(position.y, 0, screensize.y)
+
+
